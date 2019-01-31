fa529fbfe7d14285f7fd677904cf2568e9622859
Blazej Doleska <blazej.doleska@creativestyle.pl>
2019-01-31 14:46:01 +0100

    fix to check if table exist

--- Plugin/RegenerateWarmupUrls.php
+++ Plugin/RegenerateWarmupUrls.php
@@ -11,19 +11,26 @@ class RegenerateWarmupUrls
      * @var \MageSuite\PageCacheWarmer\Model\ResourceModel\WarmupQueue\Url\CollectionFactory
      */
     private $pageWarmerCollectionFactory;
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    private $resourceConnection;
 
     public function __construct(
         \MageSuite\PageCacheWarmer\Service\CronScheduler $cronScheduler,
-        \MageSuite\PageCacheWarmer\Model\ResourceModel\WarmupQueue\Url\CollectionFactory $pageWarmerCollectionFactory
+        \MageSuite\PageCacheWarmer\Model\ResourceModel\WarmupQueue\Url\CollectionFactory $pageWarmerCollectionFactory,
+        \Magento\Framework\App\ResourceConnection $resourceConnection
     )
     {
         $this->cronScheduler = $cronScheduler;
         $this->pageWarmerCollectionFactory = $pageWarmerCollectionFactory;
+        $this->resourceConnection = $resourceConnection;
     }
 
     public function afterCleanType(\Magento\Framework\App\Cache\TypeList $subject, $result, $typeCode)
     {
-        if ($typeCode == 'full_page') {
+        $isTableExist = $this->resourceConnection->getConnection()->isTableExists('cache_warmup_queue');
+        if ($typeCode == 'full_page' && $isTableExist) {
             $pageWarmerCollection = $this->pageWarmerCollectionFactory->create();
 
             $pageWarmerCollection->walk('delete');
